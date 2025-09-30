2a9148f422a793e314bb13d49f76a711ab109d43
Yavar <zuri00014@gmail.com>
2025-10-01 02:03:06 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+import NavBar from './components/NavBar/NavBar'
+
+const App = () => {
+  return (
+    <div className='main-container-home-page app'>
+      <NavBar/>
+      <video autoPlay loop muted playsInline className="bg-video">
+        <source src="/public/ChatGPT  The Intelligence Age - OpenAI (1080p, h264).mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./NavBar.css"
+const NavBar = () => {
+  return (
+    <div className='nav-container'>
+      <div className="LogoText">
+      Yavar
+      </div>
+      <div className="navOptions">
+        <div>Home</div>
+        <div>About</div>
+        <div>Work</div>
+        <div>Contact</div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
