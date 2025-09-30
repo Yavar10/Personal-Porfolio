21148e176fe4407f96700356629bcba8e694d47c
Yavar <zuri00014@gmail.com>
2025-10-01 02:06:34 +0530

    fix: update video source path in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className='main-container-home-page app'>
       <NavBar/>
       <video autoPlay loop muted playsInline className="bg-video">
-        <source src="/public/ChatGPT  The Intelligence Age - OpenAI (1080p, h264).mp4" type="video/mp4" />
+        <source src="/ChatGPT  The Intelligence Age - OpenAI (1080p, h264).mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
