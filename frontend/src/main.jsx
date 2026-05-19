e3e14d79883b4d8c5b400cc3d42f78b1b8ded39b
Yavar <zuri00014@gmail.com>
2026-05-19 12:31:18 +0530

    smooth scroll

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import SmoothScroll from './components/ui/SmoothScroll.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <SmoothScroll />
     <App />
   </StrictMode>,
 )
